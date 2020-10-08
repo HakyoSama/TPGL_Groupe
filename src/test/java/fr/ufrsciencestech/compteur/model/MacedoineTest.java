4c7a4230313bc2289b48514d490ca4175af3050e
SebastienGuillemin <sebastine.guillemin11@gmail.com>
2020-10-08 11:53:46 +0200

    Test Macedoine

---
+++ src/test/java/fr/ufrsciencestech/compteur/model/MacedoineTest.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ufrsciencestech.compteur.model;
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author sg255589
+ */
+public class MacedoineTest {
+    private ArrayList<Fruit> fruits;
+    private boolean seedless;
+    
+    public MacedoineTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+     public void testAjoute(Fruit f)
+      {}
+     
+     @Ignore
+     public void testToString()
+      {}
+     
+      @Test
+      public void testGetPrix()
+      {}
+      
+      @Test
+      public void testIsSeedless()
+      {}
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
